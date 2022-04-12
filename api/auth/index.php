4588d7e91baae0c9b015d919098f0e32503327a9
Brad Bartlett <box1@bradleyb.us>
2022-04-12 17:21:15 -0400

    add origin handler for api auth

--- api/auth/index.php
+++ api/auth/index.php
@@ -3,7 +3,24 @@
 	require "$root/config.php";
 	require "$root/include/wine.php";
 	   
+	if (isset($_SERVER['HTTP_ORIGIN'])) {
+		header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+		header('Access-Control-Allow-Credentials: true');
+		header('Access-Control-Max-Age: 86400');    // cache for 1 day
+	}
+
    $method = $_SERVER['REQUEST_METHOD'];
+	if (strtoupper($method) == 'OPTIONS') {
+		
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+			header("Access-Control-Allow-Methods: POST, OPTIONS");
+		
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+			header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+		exit(0);
+	}
+
    if (strtoupper($method) == 'POST') {
       $body = file_get_contents('php://input');
       $auth = json_decode($body, true);
